81268c9df3c855b332051e0537bd99b8a538ba21
kyohoonsim <kyohoonsim@gmail.com>
2022-08-20 00:21:57 +0900

    첫버전

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="(a, i) in 메뉴들" :key="i">{{ a }}</a>
+  </div>
+
+  <!-- <div v-for="(a, i) in products" :key="i">
+    <h4>{{a}}</h4>
+    <p>40만원</p>
+  </div> -->
+
+  <div v-for="(a, i) in 원룸들" :key="i">
+    <img :src="a.image" class="room-img">
+    <h4>{{a.title}}</h4>
+    <p>{{a.price}}</p>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/oneroom.js';
+
+
 
 export default {
   name: 'App',
+  data(){ // 데이터 보관함
+    return {
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수: [0, 0, 0],
+      메뉴들: ['Home', 'Products', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  methods : {
+    increase(i){
+      this.신고수[i] += 1;
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +59,42 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 </style>
